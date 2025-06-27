fc064423b433bf7c073c4af98f63cdccfcb03ab3
Gift038 <nmgift038@gmail.com>
2025-06-27 15:53:04 +0300

    CONFIGURED FIREBASE

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,44 @@
 import 'package:flutter/material.dart';
 import 'screens/welcome_page.dart';
 import 'screens/register_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    home: HostelHunt(),
-  ));
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class HostelHunt extends StatelessWidget {
-  const HostelHunt({super.key});
+  //authDomain: "hostel-hunt-d3f9d.firebaseapp.com",
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey:"AIzaSyDRS_vEqMw3HnJ9wu7BF5J_SEPowfXImWA",
+        appId: "1:358026434934:web:cd8fd7be2fdc629af14257",
+        messagingSenderId:"358026434934" ,
+        projectId: "hostel-hunt-d3f9d",
+        storageBucket: "hostel-hunt-d3f9d.firebasestorage.app",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+  
+  runApp(const HostelHuntApp());
+
+} 
+
+class HostelHuntApp extends StatelessWidget {
+  const HostelHuntApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'HostelHunt',
       debugShowCheckedModeBanner: false,
-      routes:{
+      initialRoute: '/',
+      routes: {
         '/': (context) => const WelcomePage(),
         '/register': (context) => const RegisterPage(),
       },
     );
   }
 }
-
-
